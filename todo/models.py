c3068f4e6bae97c3535a05305d807779a12c7ba1
vs-code1 <vishantkumar19@outlook.com>
2023-08-05 14:41:50 -0700

    initial commit

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Location(models.Model):
+    locationName = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.locationName
+
+
+class Item(models.Model):
+    itemName = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    itemLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item.Name
